d143264c0419821f22b8f28c6a14efc0c8e353ac
jeff Teeters <jeff@teeters.us>
2020-08-09 21:19:12 -0500

    advance version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst") as f:
 
 setup(
     name="sphinxcontrib-filltableref",
-    version="0.1",
+    version="0.1.1",
     author="Jeff Teeters",
     author_email="jeff@teeters.us",
     license="MIT",
